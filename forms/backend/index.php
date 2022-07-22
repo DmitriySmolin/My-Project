ccad296f9a5a276eee8d61511c0bca651c4e2f73
Dmitriy Smolin <45199267+DmitriySmolin@users.noreply.github.com>
2022-07-22 20:33:28 +0400

    Add forms

---
+++ forms/backend/index.php
@@ -0,0 +1,15 @@
+<?php
+
+if(isset($_POST['password']) && isset($_POST['email'])){
+    echo 'Ваш пароль: '.$_POST['password'].'<br>';
+    echo 'Ваш email:'.$_POST['email'].'<br>';
+
+    
+}
+
+
+
+
+
+
+?>
